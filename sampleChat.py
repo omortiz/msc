63e92a9891206167c0cd591134579f7ebad34d65
Zhaomin Li <zhaominl@uci.edu>
2019-03-11 14:36:32 -0700

    split trainning code

--- sampleChat.py
+++ sampleChat.py
@@ -1,40 +1,91 @@
-'''
-Created on Mar 4, 2019
-
-@author: Isaac Li
-'''
-
 from chatterbot import ChatBot
 from chatterbot.trainers import ListTrainer
 from chatterbot.trainers import ChatterBotCorpusTrainer
+from train import trainSet1, trainSet2, trainSet3
 
-def trainChatbotSample():
+import tkinter as tk
 
-    chatbot = ChatBot("Rob")
+try:
+    import ttk as ttk
+    import ScrolledText
+except ImportError:
+    import tkinter.ttk as ttk
+    import tkinter.scrolledtext as ScrolledText
+import time
 
-    trainer = ListTrainer(chatbot)
 
-    trainer.train("chatterbot.corpus.english")
-    
-    return(chatbot)
+class TkinterGUIExample(tk.Tk):
 
-if __name__=="__main__":
-    #chatbot = trainChatbotSample()
-    chatbot = ChatBot("Rob")
+    def __init__(self, *args, **kwargs):
+        """
+        Create & set window variables.
+        """
+        tk.Tk.__init__(self, *args, **kwargs)
 
-    trainer = ListTrainer(chatbot)
+        self.chatbot = ChatBot(
+            "Rob.ai",
+            storage_adapter="chatterbot.storage.SQLStorageAdapter",
+            logic_adapters=[
+                "chatterbot.logic.BestMatch"
+            ],
+            database_uri="sqlite:///database.db"
+        )
 
-    trainer.train("chatterbot.corpus.english")
-    
-    print("Please type your question or 'quit'\n")
-    inp = input("Me: \n")
-    
-    while (inp != 'quit'):
+        # Greetings
+        trainer = ChatterBotCorpusTrainer(self.chatbot)
+        trainer.train(
+            "chatterbot.corpus.english.greetings",
+            "chatterbot.corpus.english.conversations"
+        )
+
+        # Greeting Questions
+        trainSet1(self.chatbot)
         
-        response = chatbot.get_response(inp)
-        print('Rob:')
-        print(response)
-        print(" ")
-        inp = input("Me: \n")
-    
-    
+        # HR Questions from Niijan
+        trainSet2(self.chatbot)
+        
+        # HR Questions 3
+        trainSet3(self.chatbot)
+        
+        self.title("Chatterbot")
+
+        self.initialize()
+
+    def initialize(self):
+        """
+        Set window layout.
+        """
+        self.grid()
+
+        self.respond = ttk.Button(self, text='Get Response', command=self.get_response)
+        self.respond.grid(column=0, row=0, sticky='nesw', padx=3, pady=3)
+
+        self.usr_input = ttk.Entry(self, state='normal')
+        self.usr_input.grid(column=1, row=0, sticky='nesw', padx=3, pady=3)
+
+        self.conversation_lbl = ttk.Label(self, anchor=tk.E, text='Conversation:')
+        self.conversation_lbl.grid(column=0, row=1, sticky='nesw', padx=3, pady=3)
+
+        self.conversation = ScrolledText.ScrolledText(self, state='disabled')
+        self.conversation.grid(column=0, row=2, columnspan=2, sticky='nesw', padx=3, pady=3)
+
+    def get_response(self):
+        """
+        Get a response from the chatbot and display it.
+        """
+        user_input = self.usr_input.get()
+        self.usr_input.delete(0, tk.END)
+
+        response = self.chatbot.get_response(user_input)
+
+        self.conversation['state'] = 'normal'
+        self.conversation.insert(
+            tk.END, "Human: " + user_input + "\n" + "ChatBot: " + str(response.text) + "\n\n"
+        )
+        self.conversation['state'] = 'disabled'
+
+        time.sleep(0.5)
+
+
+gui_example = TkinterGUIExample()
+gui_example.mainloop()
---
+++ train.py
@@ -0,0 +1,139 @@
+
+from chatterbot.trainers import ListTrainer
+from multiprocessing.connection import Listener
+
+HRdataList = [["How do I find out about job openings?",
+"Visit our career site. Before applying we ask that you carefully read the job description to ensure you fulfill the requirements and are interested in the position."],
+["How long are jobs posted?",
+"Job openings are posted for a minimum of five business days or longer, depending on the position."],
+["How do I apply for a job?",
+"You must apply online to be considered. Click the 'Apply for this job'  button located next to the job listing."],
+["How long will my application be kept online?",
+"Your application material will be retained for at least one year. "],
+["How can I update an application or cover letter that I previously turned in?",
+"Job specific questions cannot be changed once submitted."],
+["How can I update an application or cover letter that I previously turned in?",
+"Profile level questions can be changed at any time by using the link on the candidate profile."],
+["What is Employee Child Care Assistance Program? (ECCAP)",
+"ECCAP is a program offered to assist faculty and staff to meet the cost of childcare."],
+["My doctor does not accept Medicare. Will I still receive reimbursement from the Medicare Plan?",
+"If your doctor does not accept Medicare, the plan will reimburse you as if Medicare paid its portion of the expense. "],
+["What is the University's short term Disability Policy?",
+"If your none work-related illness or injury is serious and requires you to be absent for 8 or more consecutive calendar days, you can apply for short term disability leave."],
+["What will I be paid?",
+"Full base pay is up to 12 weeks and 75% of base salary for an additional 14 weeks."],
+["What should I do if I do not come to work?",
+"You should notify your supervisor as soon as possible if you cannot or choose not to report to work at all. The entire day is charged to leave without pay or, if your supervisor approves, paid time off."]]
+
+def trainSet1(bot):
+    trainer = ListTrainer(bot)
+
+    trainer.train([
+    "Hi there!",
+    "Hello",
+    ])
+
+    trainer.train([
+    "Greetings!",
+    "Hello",
+    ])
+
+    trainer.train([
+    "Are you a human?",
+    "I'm a digital assistant.",
+    ])
+
+    trainer.train([
+    "What are you",
+    "I'm a digital assistant.",
+    ])
+
+    trainer.train([
+    'How can I help you?',
+    'I want to create a chat bot',
+    'Have you read the documentation?',
+    'No, I have not',
+    'This should help get you started: http://chatterbot.rtfd.org/en/latest/quickstart.html'
+    ])
+
+def trainSet2(bot):
+    trainer = ListTrainer(bot)
+    
+    for pair in HRdataList:
+        trainer.train(pair)
+        
+def trainSet3(bot):
+    trainer = ListTrainer(bot)
+    
+    trainer.train([
+            "Hi, can I help you?",
+            "Yes, I would like information about my 401K?",
+            "Here is a link where you can find information about your 401K: https://www.irs.gov/retirement-plans/401k-plans"
+            "Thanks Rob!",
+            "You're welcome!"
+        ])
+
+    trainer.train([
+            "Hi there! How can I help you?",
+            "I need to know who is the CEO of the company",
+            "The current CEO of MSC Software is Paolo Guglielmini."
+            "Thanks Rob!",
+            "You're welcome!"
+        ])
+
+    trainer.train([
+            "Hi there! How can I help you?",
+            "Who is the current CFO?",
+            "The current CFO of MSC Software is Kevin Rubin."
+            "Thanks Rob!",
+            "You're welcome!"
+        ])
+
+    trainer.train([
+            "How can I help you?",
+            "When was the company founded?",
+            "MSC Software was founded in 1963.",
+            "Thanks Rob!",
+            "You're welcome!"
+        ])
+
+    trainer.train([
+            "Hi there! How can I help you?",
+            "When was MSC Software founded?",
+            "MSC Software was founded in 1963.",
+            "Thanks Rob!",
+            "You're welcome!"
+        ])
+
+    trainer.train([
+            "Hello! How can I help you?"
+            "What's the address of MSC Software?",
+            "MSC Software is located at 4675 MacArthur Court Newport Beach, CA 92660.",
+            "Okay thanks!",
+            "Your're welcome! Is there anything else I can do for you?",
+            "No, thanks!"
+        ])
+
+    trainer.train([
+            "Hi! How can I help you?",
+            "What's the company's address?",
+            "MSC Software is located at 4675 MacArthur Court Newport Beach, CA 92660.",
+            "Do you need anything else?",
+            "No, thank you!",
+            "Have a good day!",
+            "Thanks, you too!"
+        ])
+
+    trainer.train([
+            "Hi! How can I help you?",
+            "What's the phone number of MSC Software?",
+            "The phone number for MSC Software (714) 540-8900.",
+
+        ])
+
+    trainer.train([
+            "Hi! How can I help you?",
+            "What's MSC Software's phone number?",
+            "The phone number for MSC Software (714) 540-8900.",
+
+        ])

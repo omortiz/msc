f9a03279fa43d902016f6bc65e289e8166ac5c52
Oscar Ortiz <omortiz@uci.edu>
2019-03-11 13:02:09 -0700

    Sample GUI
    Added sample GUI using Tkinter

--- sampleChat.py
+++ sampleChat.py
@@ -1,40 +1,70 @@
-'''
-Created on Mar 4, 2019
-
-@author: Isaac Li
-'''
-
 from chatterbot import ChatBot
-from chatterbot.trainers import ListTrainer
-from chatterbot.trainers import ChatterBotCorpusTrainer
+import tkinter as tk
+try:
+    import ttk as ttk
+    import ScrolledText
+except ImportError:
+    import tkinter.ttk as ttk
+    import tkinter.scrolledtext as ScrolledText
+import time
 
-def trainChatbotSample():
 
-    chatbot = ChatBot("Rob")
+class TkinterGUIExample(tk.Tk):
 
-    trainer = ListTrainer(chatbot)
+    def __init__(self, *args, **kwargs):
+        """
+        Create & set window variables.
+        """
+        tk.Tk.__init__(self, *args, **kwargs)
 
-    trainer.train("chatterbot.corpus.english")
-    
-    return(chatbot)
+        self.chatbot = ChatBot(
+            "GUI Bot",
+            storage_adapter="chatterbot.storage.SQLStorageAdapter",
+            logic_adapters=[
+                "chatterbot.logic.BestMatch"
+            ],
+            database_uri="sqlite:///database.db"
+        )
 
-if __name__=="__main__":
-    #chatbot = trainChatbotSample()
-    chatbot = ChatBot("Rob")
+        self.title("Chatterbot")
 
-    trainer = ListTrainer(chatbot)
+        self.initialize()
 
-    trainer.train("chatterbot.corpus.english")
-    
-    print("Please type your question or 'quit'\n")
-    inp = input("Me: \n")
-    
-    while (inp != 'quit'):
-        
-        response = chatbot.get_response(inp)
-        print('Rob:')
-        print(response)
-        print(" ")
-        inp = input("Me: \n")
-    
-    
+    def initialize(self):
+        """
+        Set window layout.
+        """
+        self.grid()
+
+        self.respond = ttk.Button(self, text='Get Response', command=self.get_response)
+        self.respond.grid(column=0, row=0, sticky='nesw', padx=3, pady=3)
+
+        self.usr_input = ttk.Entry(self, state='normal')
+        self.usr_input.grid(column=1, row=0, sticky='nesw', padx=3, pady=3)
+
+        self.conversation_lbl = ttk.Label(self, anchor=tk.E, text='Conversation:')
+        self.conversation_lbl.grid(column=0, row=1, sticky='nesw', padx=3, pady=3)
+
+        self.conversation = ScrolledText.ScrolledText(self, state='disabled')
+        self.conversation.grid(column=0, row=2, columnspan=2, sticky='nesw', padx=3, pady=3)
+
+    def get_response(self):
+        """
+        Get a response from the chatbot and display it.
+        """
+        user_input = self.usr_input.get()
+        self.usr_input.delete(0, tk.END)
+
+        response = self.chatbot.get_response(user_input)
+
+        self.conversation['state'] = 'normal'
+        self.conversation.insert(
+            tk.END, "Human: " + user_input + "\n" + "ChatBot: " + str(response.text) + "\n"
+        )
+        self.conversation['state'] = 'disabled'
+
+        time.sleep(0.5)
+
+
+gui_example = TkinterGUIExample()
+gui_example.mainloop()

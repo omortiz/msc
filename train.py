3d3202f02d11a9c36b65053923026428e0c2160a
Zhaomin Li <zhaominl@uci.edu>
2019-03-12 02:35:11 -0700

    solve conflict in train

--- train.py
+++ train.py
@@ -1,39 +1,3 @@
-<<<<<<< HEAD
-
-<<<<<<< HEAD
-from chatterbot.trainers import ListTrainer
-from multiprocessing.connection import Listener
-
-HRdataList = [["How do I find out about job openings?",
-"Visit our career site. Before applying we ask that you carefully read the job description to ensure you fulfill the requirements and are interested in the position."],
-["How long are jobs posted?",
-"Job openings are posted for a minimum of five business days or longer, depending on the position."],
-["How do I apply for a job?",
-"You must apply online to be considered. Click the 'Apply for this job'  button located next to the job listing."],
-["How long will my application be kept online?",
-"Your application material will be retained for at least one year. "],
-["How can I update an application or cover letter that I previously turned in?",
-"Job specific questions cannot be changed once submitted."],
-["How can I update an application or cover letter that I previously turned in?",
-"Profile level questions can be changed at any time by using the link on the candidate profile."],
-["What is Employee Child Care Assistance Program? (ECCAP)",
-"ECCAP is a program offered to assist faculty and staff to meet the cost of childcare."],
-["My doctor does not accept Medicare. Will I still receive reimbursement from the Medicare Plan?",
-"If your doctor does not accept Medicare, the plan will reimburse you as if Medicare paid its portion of the expense. "],
-["What is the University's short term Disability Policy?",
-"If your none work-related illness or injury is serious and requires you to be absent for 8 or more consecutive calendar days, you can apply for short term disability leave."],
-["What will I be paid?",
-"Full base pay is up to 12 weeks and 75% of base salary for an additional 14 weeks."],
-["What should I do if I do not come to work?",
-"You should notify your supervisor as soon as possible if you cannot or choose not to report to work at all. The entire day is charged to leave without pay or, if your supervisor approves, paid time off."]]
-
-def trainSet1(bot):
-    trainer = ListTrainer(bot)
-
-=======
-
-=======
->>>>>>> 9b485ab4afee2c032417e2b7c66ed196a914df10
 from chatterbot.trainers import ListTrainer
 from chatterbot.trainers import ChatterBotCorpusTrainer
 from multiprocessing.connection import Listener
@@ -69,7 +33,6 @@ def trainSet1(bot):
 
     trainer = ListTrainer(bot)
 
->>>>>>> 035d2668b774adc7aa50e06c9c991a0fc26b2cf8
     trainer.train([
         "Hi there!",
         "Hello",
@@ -475,21 +438,6 @@ def trainSet3(bot):
         "MSC Software Corporation is an American software company based in Newport Beach, California, that specializes in simulation software."
     ])
 
-
-
-    trainer.train([
-        "Hi! How can I help you?",
-        "What's MSC Software?",
-        "MSC Software Corporation is an American software company. Here's a link to it's Wikipedia page: https://en.wikipedia.org/wiki/MSC_Software",
-        "Okay, thanks!",
-        "You're welcome!"
-    ])
-
-<<<<<<< HEAD
-        ])
-<<<<<<< HEAD
-=======
-=======
     trainer.train([
         "What are some products made by MSC Software?",
         "MSC Software has created the following products: MSC Apex, Actran, Adams, Digimat, Dytran, Easy5, Marc, MaterialCenter, MSC Fatigue, MSC Nastran, Patran, SimDesigner, SimManager, SimXpert, and Sinda"
@@ -609,6 +557,3 @@ def trainSet3(bot):
         "What does company work on?",
         "MSC Software has created the following products: MSC Apex, Actran, Adams, Digimat, Dytran, Easy5, Marc, MaterialCenter, MSC Fatigue, MSC Nastran, Patran, SimDesigner, SimManager, SimXpert, and Sinda"
     ])
->>>>>>> 9b485ab4afee2c032417e2b7c66ed196a914df10
-
->>>>>>> 035d2668b774adc7aa50e06c9c991a0fc26b2cf8
